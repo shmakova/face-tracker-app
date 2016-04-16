f943c380e7115fd2ec4a96e66050452f0d6b66bc
shmakova <shmakova-nastya@yandex.ru>
2016-04-16 16:11:15 +0600

    Initial Commit

---
+++ FaceTrackerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceTrackerApp
+//
+//  Created by Anastasia Shmakova on 16.04.16.
+//  Copyright © 2016 Shmakova Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
